8c7b2cc3e086320536824588096515f2a19a2196
juveyn <739568@newcollege.ac.uk>
2025-02-14 16:59:29 +0000

    movement
    yes

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -7,10 +8,11 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed;
     public float jumpForce;
+    public CollectableManager cm;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,11 +21,22 @@ public class PlayerMovement : MonoBehaviour
         float movement = Input.GetAxis("Horizontal");
         Vector2 direction = new Vector2(movement, 0);
 
-        transform.Translate(direction* speed * Time.deltaTime);
+        transform.Translate(direction * speed * Time.deltaTime);
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            GetComponent<Rigidbody2D>().AddForce(new Vector2(0,jumpForce * Time.deltaTime));
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce * Time.deltaTime));
         }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Flower"))
+        {
+            Destroy(collision.gameObject);
+            cm.flowerCount++;
+
+        }
+
     }
 }
